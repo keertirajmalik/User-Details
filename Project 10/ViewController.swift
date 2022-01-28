312bb15731f8367f0192843cfb21f3c7d0bcb0be
keertirajmalik <keertirajmalik@gmail.com>
2022-01-28 16:54:55 +0530

    Initial Commit

---
+++ Project 10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 10
+//
+//  Created by Keertiraj Laxman Malik on 28/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
