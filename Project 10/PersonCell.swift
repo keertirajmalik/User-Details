03b1a175565cbc95bb2e026a1f015cf99e69e735
keertirajmalik <keertirajmalik@gmail.com>
2022-01-28 17:11:41 +0530

    creating new class  for person cell and connect the outlets

---
+++ Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Keertiraj Laxman Malik on 28/01/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
