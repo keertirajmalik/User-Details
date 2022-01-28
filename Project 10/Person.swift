e0d5fed7ad0a6ecdcef6275f2d686c5573dd410f
keertirajmalik <keertirajmalik@gmail.com>
2022-01-28 23:15:42 +0530

    person class to hold all the image and name details

---
+++ Project 10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Keertiraj Laxman Malik on 28/01/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
